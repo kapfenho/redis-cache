299fbb709fdabea05db321a3f7dcfa8c20b42686
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2012-12-21 10:12:58 +0100

    structure works

--- src/import.rb
+++ src/import.rb
@@ -2,13 +2,14 @@ require 'rubygems'
 require 'redis'
 
 db = Redis.new
-File.open('data/names.txt', "r").each_line do |l|
+File.open('data/fullset-cons.csv', "r").each_line do |l|
   l1 = l.gsub /"/, ''
   a = l1.split(',')
-  if a.size != 3 
+  if a.size != 6 
     puts 'Error for: #{l}'
     next
   end
+  db.set 
   db.lpush a[0].downcase, a[2]
 end
 #    db.lpush i, products[Random.rand(0..11)]
