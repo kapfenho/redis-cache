a2aba9f6ae341f87f1434a7f1b369d3a6710cf17
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2012-12-21 17:26:01 +0100

    data model, native protocol

--- src/import.rb
+++ src/import.rb
@@ -1,18 +1,47 @@
-require 'rubygems'
-require 'redis'
+##### redis-cache.rb
+#
+# Fill redis cache with data
+#
+# Author: horst kapfenberger
+#
 
-db = Redis.new
-File.open('data/fullset-cons.csv', "r").each_line do |l|
-  l1 = l.gsub /"/, ''
-  a = l1.split(',')
-  if a.size != 6 
-    puts 'Error for: #{l}'
-    next
+%w[rubygems redis json logger benchmark].each { |r| require r }
+require_relative 'gen_redis_proto'
+
+ckeys = [:id, :msisdn, :acct, :fn, :ln, :zip]
+i = 0
+mode = :file  # :db
+out = ""
+
+logger = Logger.new(STDERR)
+logger.level = Logger::INFO
+
+logger.info Benchmark.measure {
+
+  db = Redis.new
+  File.open('data/fullset-cons.csv', "r").each_line do |l|
+    a = l.strip!.gsub(/"/, '').split(',')
+
+    if a.size != 6 
+      logger.error "Invalid data in row #{i}: #{l}"
+    else
+      h = Hash[ckeys.zip(a)]
+      if mode == :file
+        out << gen_redis_proto('SET', "cons:#{h[:id]}", h.to_json)
+        out << gen_redis_proto('HSET', 'cons:lookup:msisdn', h[:msisdn], h[:id])
+        out << gen_redis_proto('HSET', 'cons:lookup:acct', h[:acct], h[:id])
+        out << gen_redis_proto('SADD',  "cons:ln:#{h[:ln]}", h[:id])
+      else
+        db.set "cons:#{h[:id]}", h.to_json
+        db.hset 'cons:lookup:msisdn', h[:msisdn], h[:id]
+        db.hset 'cons:lookup:acct', h[:acct], h[:id]
+        db.sadd "cons:ln:#{h[:ln]}", h[:id]
+      end
+    end
+    i += 1
+
   end
-  db.set 
-  db.lpush a[0].downcase, a[2]
-end
-#    db.lpush i, products[Random.rand(0..11)]
+  puts out
 
-# db['Horst'] = '42'
-# p db['Horst']
+}
+
